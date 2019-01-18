ac332c097e5a021fec25500492ebdccb264ee74b
Alex Webb <alexwebbb@protonmail.com>
2019-01-18 12:16:20 -0600

    program stubs

---
+++ libSafe.bash
@@ -0,0 +1,23 @@
+#!bin/bash/
+
+# general function library for safe-remove
+
+function list () {
+	echo "There is nothing here yet!"
+}
+
+function check () {
+	echo "There is nothing here yet!"
+}
+
+function isError () {
+	echo "There is nothing here yet!"
+}
+
+function interact () {
+	echo "There is nothing here yet!"
+}
+
+function verbose() {
+	echo "There is nothing here yet!"
+} 
---
+++ remove.bash
@@ -0,0 +1,8 @@
+#!bin/bash/
+
+# remove function
+
+. ./libSafe
+
+read
+
